7e9432a45cce6b99ccc64bac5ad72c624056461a
Nicolae Dilan <nicolae.dilan@amdaris.com>
2018-07-03 13:11:30 +0300

    345_fix_first_change_on_master

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("First vhange on master");
+            Console.WriteLine("First change on master");
             Console.ReadKey();
         }
     }
