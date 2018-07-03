5c909554f94dcab008e9e0da45d3504803f527cd
Nicolae Dilan <nicolae.dilan@amdaris.com>
2018-07-03 13:58:57 +0300

    second_commit_from_stash

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -18,7 +18,7 @@ namespace ConsoleApp1
             Console.WriteLine("First change on 123_first_branch");
             Console.WriteLine("Second change on 123_first_branch");
             Console.WriteLine("Merge and save all changes");
-            Console.WriteLine("Changes for second stash");
+            Console.WriteLine("Make some changes for stash");
             Console.ReadKey();
         }
     }
