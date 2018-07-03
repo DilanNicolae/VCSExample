528d6bbdabd5cfef29bca79b5b7f63af9bd8ad86
Nicolae Dilan <nicolae.dilan@amdaris.com>
2018-07-03 12:55:28 +0300

    123_commit_first_change

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("First vhange on master");
             Console.ReadKey();
         }
     }
